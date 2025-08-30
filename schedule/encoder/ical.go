c9f8f9805dc8476297d87086c0cf25ed305151de
Vladimir <74735618+qsoulior@users.noreply.github.com>
2025-08-30 15:05:11 +0300

    change LF to CRLF in ical encoder

--- schedule/encoder/ical.go
+++ schedule/encoder/ical.go
@@ -32,59 +32,59 @@ func NewIcal(w io.Writer) Encoder { return &icalEncoder{w} }
 // Encode encodes schedule events and group into ical format.
 // It uses subgroup to filter events.
 func (e *icalEncoder) Encode(events []schedule.Event, group string, subgroup schedule.EventSubgroup) error {
-	fmt.Fprint(e.w, "BEGIN:VCALENDAR\n")
+	fmt.Fprint(e.w, "BEGIN:VCALENDAR\r\n")
 
 	// VCALENDAR
-	fmt.Fprintf(e.w, "PRODID:%s\n", icalProductID)
-	fmt.Fprintf(e.w, "VERSION:%s\n", icalVersion)
-	fmt.Fprintf(e.w, "METHOD:%s\n", icalMethod)
-	fmt.Fprintf(e.w, "CALSCALE:%s\n", icalScale)
+	fmt.Fprintf(e.w, "PRODID:%s\r\n", icalProductID)
+	fmt.Fprintf(e.w, "VERSION:%s\r\n", icalVersion)
+	fmt.Fprintf(e.w, "METHOD:%s\r\n", icalMethod)
+	fmt.Fprintf(e.w, "CALSCALE:%s\r\n", icalScale)
 
 	// VTIMEZONE
-	fmt.Fprint(e.w, "BEGIN:VTIMEZONE\n")
-	fmt.Fprintf(e.w, "TZID:%s\n", icalTimezoneID)
-	fmt.Fprint(e.w, "BEGIN:STANDARD\n")
-	fmt.Fprint(e.w, "DTSTART:19700101T000000\n")
-	fmt.Fprintf(e.w, "TZOFFSETFROM:%s\n", icalTimezoneOffset)
-	fmt.Fprintf(e.w, "TZOFFSETTO:%s\n", icalTimezoneOffset)
-	fmt.Fprint(e.w, "END:STANDARD\n")
-	fmt.Fprint(e.w, "END:VTIMEZONE\n")
+	fmt.Fprint(e.w, "BEGIN:VTIMEZONE\r\n")
+	fmt.Fprintf(e.w, "TZID:%s\r\n", icalTimezoneID)
+	fmt.Fprint(e.w, "BEGIN:STANDARD\r\n")
+	fmt.Fprint(e.w, "DTSTART:19700101T000000\r\n")
+	fmt.Fprintf(e.w, "TZOFFSETFROM:%s\r\n", icalTimezoneOffset)
+	fmt.Fprintf(e.w, "TZOFFSETTO:%s\r\n", icalTimezoneOffset)
+	fmt.Fprint(e.w, "END:STANDARD\r\n")
+	fmt.Fprint(e.w, "END:VTIMEZONE\r\n")
 
 	// VEVENT
 	for _, event := range events {
 		if subgroup == "" || event.Subgroup == "" || subgroup == event.Subgroup {
 			e.encodeEvent(event)
-			fmt.Fprint(e.w, "\n")
+			fmt.Fprint(e.w, "\r\n")
 		}
 	}
 
-	fmt.Fprint(e.w, "END:VCALENDAR\n")
+	fmt.Fprint(e.w, "END:VCALENDAR\r\n")
 	return nil
 }
 
 func (e *icalEncoder) encodeEvent(event schedule.Event) {
-	fmt.Fprint(e.w, "BEGIN:VEVENT\n")
+	fmt.Fprint(e.w, "BEGIN:VEVENT\r\n")
 
-	fmt.Fprintf(e.w, "UID:%s\n", uuid.New())
-	fmt.Fprintf(e.w, "DTSTAMP:%s\n", time.Now().UTC().Format(icalLayoutDatetimeUTC))
-	fmt.Fprintf(e.w, "LOCATION:%s\n", event.Location)
-	fmt.Fprintf(e.w, "TRANSP:%s\n", icalTransparent)
+	fmt.Fprintf(e.w, "UID:%s\r\n", uuid.New())
+	fmt.Fprintf(e.w, "DTSTAMP:%s\r\n", time.Now().UTC().Format(icalLayoutDatetimeUTC))
+	fmt.Fprintf(e.w, "LOCATION:%s\r\n", event.Location)
+	fmt.Fprintf(e.w, "TRANSP:%s\r\n", icalTransparent)
 
 	fmt.Fprint(e.w, "SUMMARY:")
 	if event.Subgroup != "" {
 		fmt.Fprintf(e.w, "[%s] ", event.Subgroup)
 	}
-	fmt.Fprintf(e.w, "%s\n", event.Title)
+	fmt.Fprintf(e.w, "%s\r\n", event.Title)
 
 	fmt.Fprintf(e.w, "DESCRIPTION:%s [%s", event.Teacher, event.Type)
 	if event.Subgroup != "" {
 		fmt.Fprintf(e.w, " - %s", event.Subgroup)
 	}
-	fmt.Fprint(e.w, "]\n")
+	fmt.Fprint(e.w, "]\r\n")
 
-	fmt.Fprintf(e.w, "DTSTART;TZID=%s:%s\n",
+	fmt.Fprintf(e.w, "DTSTART;TZID=%s:%s\r\n",
 		icalTimezoneID, event.Dates[0].Start.Format(icalLayoutDatetime))
-	fmt.Fprintf(e.w, "DTEND;TZID=%s:%s\n",
+	fmt.Fprintf(e.w, "DTEND;TZID=%s:%s\r\n",
 		icalTimezoneID, event.Dates[0].End.Format(icalLayoutDatetime))
 
 	dates := make([]string, len(event.Dates))
@@ -92,7 +92,7 @@ func (e *icalEncoder) encodeEvent(event schedule.Event) {
 		dates[i] = date.Start.UTC().Format(icalLayoutDatetimeUTC)
 	}
 
-	fmt.Fprintf(e.w, "RDATE:%s\n", strings.Join(dates, ","))
+	fmt.Fprintf(e.w, "RDATE:%s\r\n", strings.Join(dates, ","))
 
 	fmt.Fprint(e.w, "END:VEVENT")
 }
